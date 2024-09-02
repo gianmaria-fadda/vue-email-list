a92deb97827e92db7cb5db9cf1dc3706d8417aa7
Gianmaria Fadda <faddagianmaria10@gmail.com>
2024-09-02 14:22:39 +0200

    Creato ciclo email

--- js/script.js
+++ js/script.js
@@ -3,14 +3,18 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
-        message: 'Vue Email List'
+        message: 'Vue Email List',
+        email: ''
       };
     },
     created() {
-      axios
-          .get('https://flynn.boolean.careers/exercises/api/random/mail')
-          .then(function () {
-            
-          });
+      for (let i = 0; i < 5; i++) {
+        axios
+            .get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((response)=> {
+              console.log(i + ": " + response.data.response);
+              this.email = response.data.response;
+            });
+      }
     }
   }).mount('#app');
